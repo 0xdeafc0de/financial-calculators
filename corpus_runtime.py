f0dfa80a0832c22e2c711d0541ddc0929f66aaa6
Sanjay Pingal <sanjaypingal@gmail.com>
2025-05-30 21:13:28 +0530

    default values provided

--- corpus_runtime.py
+++ corpus_runtime.py
@@ -4,11 +4,16 @@
 #
 import numpy as np
 
-# user inputs
-corpus = float(input("Enter initial corpus amount (e.g., 20000000 for ₹2Cr): "))
-yearly_exp = float(input("Enter yearly withdrawl amount (e.g., 1200000 for ₹1L/month): "))
-roi = float(input("Enter avg annual RoI (in %, e.g., 7.5: ")) / 100
-inflation_rate = float(input("Enter avg inflation rate (in %, e.g., 5): ")) / 100
+# user inputs with default (if no input provided)
+corpus = float(input("Enter initial corpus amount (e.g., 20000000 for ₹2Cr): ").strip() or 20000000)
+yearly_exp = float(input("Enter yearly withdrawl amount (e.g., 1200000 for ₹1L/month): ").strip() or 1200000)
+roi = float(input("Enter avg annual RoI (in %, e.g., 7.5: ").strip() or 8)
+inflation_rate = float(input("Enter avg inflation rate (in %, e.g., 5): ").strip() or 5)
+
+print(f"Using corpus {corpus}, yearly expenses {yearly_exp}, rate-of-int {roi}%/yr and inflation rate {inflation_rate}%")
+
+roi = roi / 100
+inflation_rate = inflation_rate / 100
 
 # Convert annual to monthly rates
 monthly_roi = (1 + roi) ** (1/12) - 1
